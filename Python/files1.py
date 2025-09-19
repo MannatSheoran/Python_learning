366815aafb981d3802171c1c0dc5e8089794f428
MannatSheoran <mannatsheoran864@gmail.com>
2025-09-19 12:09:39 +0530

    Add files1.py

--- Python/files1.py
+++ Python/files1.py
@@ -2,23 +2,30 @@
 # If the user enters the filename "na na boo boo", display a funny message instead.
 # Handle normal files as usual and show error if the file does not exist.
 
-
+import os
+base_dir=os.path.dirname(__file__)
 
 fint=input("Enter the filename:")
+file_path=os.path.join(base_dir,fint)
+
+
 if fint=="na na boo boo":
     print(" NA NA BOO BOO TO YOU- You have been punkd!")
     exit()
 
 try:
-    fname=open(fint)
+    with open(file_path,"r") as f:
+        count=0
+
+        for line in f:      
+            count=count+1
+
+
 except:
     print("File does not exist")
     exit()
 
-count=0
 
-for line in fname:      
-    count=count+1
     
 
 print("There were {} subject lines in {}".format(count,fint))
