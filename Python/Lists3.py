9a4098006025744c0fb9a638285660eba1fa99dd
MannatSheoran <mannatsheoran864@gmail.com>
2025-09-20 20:28:11 +0530

    Adding Lists3.py

--- Python/Lists3.py
+++ Python/Lists3.py
@@ -1,13 +1,13 @@
-
+l=[]
 while True:
     num=input("Enter the number: ")
     if num=="done":
         break
     else:
         n=int(num)
-        l=[]
+        
         l.append(n)
 
-
+print(l)
 print("The maximum number of the numbers entered is ",max(l))
-print("The maximum number of the numbers entered is ",min(l))
+print("The minimum number of the numbers entered is ",min(l))
