b08218fca8c6911e61d22a6cf5b9709b96528078
MannatSheoran <mannatsheoran864@gmail.com>
2025-09-20 20:00:02 +0530

    added one more file lists2.py

--- Python/Lists2.py
+++ Python/Lists2.py
@@ -1,2 +1,19 @@
+import os
+dir=os.path.dirname(__file__)
+fint=input("Enter the file name: ")
+file_path=os.path.join(dir,fint)
 
+try:
+    with open(file_path,"r") as f:
+        count=0
+        for i in f:
+            if i.startswith("From "):
+                print(i.split()[1])
+                count=count+1
+
+except:
+    print("The file does not exist")
+    exit()
+
+print(f"There are {count} lines in the file with From as the first word")
 
